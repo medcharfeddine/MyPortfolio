d3f6f3b844d034db366d362c7e5d642bfdf056f0
medcharfeddine <medcharfeddine@outlook.com>
2023-09-28 00:49:57 +0100

    updating navbar

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,22 +1,61 @@
-import React from 'react'
-import './nav.css'
-import {FaHome} from 'react-icons/fa'
-import {FaUserAlt} from 'react-icons/fa'
-import {FaBook} from 'react-icons/fa'
-import {RiServiceFill} from 'react-icons/ri'
-import {AiFillMessage} from 'react-icons/ai'
+/** @format */
 
+import React, { useState } from "react";
+import "./nav.css";
+import { FaHome } from "react-icons/fa";
+import { FaUserAlt } from "react-icons/fa";
+import { FaBook } from "react-icons/fa";
+import { RiServiceFill } from "react-icons/ri";
+import { AiFillMessage } from "react-icons/ai";
 
-const nav = () => {
+const Nav = () => {
+  // Create a state variable to track the active link
+  const [activeLink, setActiveLink] = useState("");
+
+  // Function to handle link clicks and set the active link
+  const handleLinkClick = (linkId) => {
+    setActiveLink(linkId);
+  };
+
   return (
     <nav>
-      <a href="#" className='active'><FaHome/></a>
-      <a href="#about"><FaUserAlt/></a>
-      <a href="#experience"><FaBook/></a>
-      <a href="#services"><RiServiceFill/></a>
-      <a href="#contact"><AiFillMessage/></a>
+      <a
+        href="#"
+        className={activeLink === "" ? "active" : ""}
+        onClick={() => handleLinkClick("")}
+      >
+        <FaHome />
+      </a>
+      <a
+        href="#about"
+        className={activeLink === "about" ? "active" : ""}
+        onClick={() => handleLinkClick("about")}
+      >
+        <FaUserAlt />
+      </a>
+      <a
+        href="#experience"
+        className={activeLink === "experience" ? "active" : ""}
+        onClick={() => handleLinkClick("experience")}
+      >
+        <FaBook />
+      </a>
+      <a
+        href="#services"
+        className={activeLink === "services" ? "active" : ""}
+        onClick={() => handleLinkClick("services")}
+      >
+        <RiServiceFill />
+      </a>
+      <a
+        href="#contact"
+        className={activeLink === "contact" ? "active" : ""}
+        onClick={() => handleLinkClick("contact")}
+      >
+        <AiFillMessage />
+      </a>
     </nav>
-  )
-}
+  );
+};
 
-export default nav
+export default Nav;
